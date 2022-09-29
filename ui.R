d00842b615ad8744fb3bff124585d8f75ac61845
Amelie <amelie.rivet2@gmail.com>
2022-09-29 16:50:50 +0200

    Brouillon 2 ui

--- ui.R
+++ ui.R
@@ -10,12 +10,32 @@ shinyUI(fluidPage(
     # Application title
     titlePanel("Welcome"),
     
+    #Choisir la region d'interet
     selectizeInput(
       inputId="RegionList", 
       label="Sélectionner une région", 
       choices=NULL, 
       options = list(maxItems = 1)
       ),
+    
+    #Choisir la taille des points des graphiques
+    sliderInput(inputId = "sizepoint", 
+                label = "Sélectionnez la taille des points des graphiques", 
+                min = 1, max = 8,
+                value = 3, step = 1),
+    
+    #Choisir la taille des titres des graphiques
+    sliderInput(inputId = "sizetitle", 
+                label = "Sélectionnez la taille des titres des graphiques", 
+                min = 1, max = 18,
+                value = 12, step = 1),
+    
+    #Choisir la taille des titres des axes des graphs
+    sliderInput(inputId = "sizetitleaxis", 
+                label = "Sélectionnez la taille des titres des axes des graphiques", 
+                min = 1, max = 18,
+                value = 10, step = 1),
+    
     
         # Affichage du graph
         mainPanel(
