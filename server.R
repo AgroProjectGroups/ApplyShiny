f529a6354b87972653c7b9749edac453e2e08ba9
Amelie <amelie.rivet2@gmail.com>
2022-09-29 21:47:11 +0200

    Brouillon 3 server

--- server.R
+++ server.R
@@ -1,11 +1,4 @@
 
-options(encoding = "UTF-8")
-library(shiny)
-library(ggplot2)
-library(plotly)
-library(dplyr)
-library(tidyverse)
-
 #Chargement du jeu de donnees
 World_Risk <- read.csv2("C:/Users/ameli/OneDrive/Documents/M2/Donnees_massives_sous_R/Projet/world_risk.csv", header=TRUE, na.strings=c("","NA"), dec=".", stringsAsFactors = TRUE)
 World_Risk$Year <- as.factor(World_Risk$Year)
@@ -37,18 +30,21 @@ shinyServer(function(input, output, session) {
     dplyr::filter(Region == input$RegionList)%>%
     select(WRI:Year)
   })
+  
   #verifier la nature des variables
   Data_final_byRegion <- reactive({
-    bind_rows("All" = Overall_average, "Region selected" = Reg_selected(), .id="Region")
+    bind_rows("Toutes les régions" = Overall_average, "Région sélectionnée" = Reg_selected(), .id="Region")
   })
   
-  output$PlotWRI <- renderPlotly({
-    p <- ggplot(Data_final_byRegion(), aes(x=Year, y=WRI, color=Region)) +
+  
+  output$Plot_by_region <- renderPlotly({
+    p <- ggplot(Data_final_byRegion(), aes_string(x="Year", y=input$yvar, group="Region", color="Region")) +
         geom_point(na.rm=TRUE, size=input$sizepoint) +
-        ggtitle("Evolution de l'indicateur WRI au cours du temps") +
+        geom_line()+
+        ggtitle(paste("Evolution de l'indicateur", input$yvar,"au cours du temps")) +
         xlab("Années") +
-        ylab("WRI")+
-        scale_color_manual(values=c("#999999", "#E69F00"))+
+        ylab(input$yvar) +
+        scale_color_manual(values=c(input$color_region,input$color_all)) +
         theme(plot.title = element_text(face="bold", hjust=0.5, size=input$sizetitle), 
               axis.title.x = element_text(size=input$sizetitleaxis, face="bold"),
               axis.title.y = element_text(size=input$sizetitleaxis, face="bold"))
